a727fb397198c59cf3a6ed3d2394c80d9b88a07f
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2015-04-05 01:40:22 -0700

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+		  	Parse.initialize("3lVtri19v6HvVPoCuT8cGkNfYflyCGU1iWG3SfAY", "jN8Oy1ff9dUc5ttbXTT4FNtUP1vcGAoK2LzwQLXe");
 
 // Declare app level module which depends on filters, and services
 angular.module('StroopGame', ['ui']);
